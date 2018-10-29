66e2984c666f8e4d487e2d310a1b6512dd08407e
Gunith Devasurendra <gunith@gmail.com>
2018-10-30 08:49:41 +1100

    Non streaming impl

---
+++ src/main/java/au/prospa/FtpDownloader.java
@@ -0,0 +1,56 @@
+package au.prospa;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.InputStreamResource;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+public class FtpDownloader {
+
+	private String server;
+	private int port = 22;
+	private String user;
+	private String password;
+
+	public FtpDownloader(String server, String user, String password) {
+		this.server = server;
+		this.user = user;
+		this.password = password;
+	}
+
+	public /*InputStreamResource*/ byte[] download(String path) throws Exception {
+		JSch jsch = new JSch();
+		Session session = null;
+		InputStream stream = null;
+		ChannelSftp sftpChannel = null;
+		
+		try {
+			session = jsch.getSession(user, server, port);
+			session.setPassword(password);
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.connect();
+
+			Channel channel = session.openChannel("sftp");
+			channel.connect();
+			sftpChannel = (ChannelSftp) channel;
+
+			stream = sftpChannel.get(path);
+			System.out.println("Downloading "+ path +" as " + user);
+			
+			return IOUtils.toByteArray(stream);
+			//return new InputStreamResource(stream);
+
+		} finally {
+			System.out.println("Finished downloading "+ path);
+			
+			stream.close();
+			sftpChannel.exit();
+			session.disconnect();
+		}
+	}
+}
--- src/main/java/au/prospa/rest/controller/FtpFetchController.java
+++ src/main/java/au/prospa/rest/controller/FtpFetchController.java
@@ -1,19 +1,47 @@
 package au.prospa.rest.controller;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.util.Base64;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import au.prospa.FtpDownloader;
+
 @RestController
 public class FtpFetchController {
-	
-	@GetMapping(value = "/file/test",produces = "audio/wav")
-	public @ResponseBody byte[] getTest() throws IOException {
-		File file = new File(getClass().getClassLoader().getResource("test.wav").getFile());
-		return Files.readAllBytes(file.toPath());
+
+	@GetMapping(value = "/file/wav/{file}", produces = "audio/wav")
+	public @ResponseBody /*ResponseEntity<InputStreamResource>*/ byte[] getWavFile(@PathVariable("file") String filePath,
+			@RequestHeader("Authorization") String authHeader) throws Exception {
+
+		String credentials = authHeader.substring(authHeader.indexOf("Basic") + "Basic".length() + 1).trim();
+		
+		String credentialsDecoded = base64Decode(credentials);
+
+		if (credentialsDecoded.indexOf(':') <= 0){
+			throw new IllegalStateException("Malformed Org header");
+		}
+		
+		String username = credentialsDecoded.substring(0, credentialsDecoded.indexOf(':'));
+		String password = credentialsDecoded.substring(username.length() + 1);
+		
+		FtpDownloader client = new FtpDownloader("sftp12.contact-world.net", username, password);
+		return client.download(base64Decode(filePath));
+		
+		/*return ResponseEntity.ok()
+                .contentType(new MediaType("audio","wav"))
+                .contentLength(len)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(resource);*/
+	}
+
+	private String base64Decode(String encoded) {
+		return new String(Base64.getDecoder().decode(encoded));
 	}
 }
